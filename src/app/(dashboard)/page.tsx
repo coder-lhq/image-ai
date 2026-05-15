2b3da1f5a9ee8527c6238afc915a7aa307322f38
coder-lhq <944905948@qq.com>
2026-05-15 15:31:52 +0800

    feat(home-template): add home templates

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -2,15 +2,16 @@ import { auth } from "@/auth"
 import { protectServer } from "@/features/auth/utils";
 import { Banner } from "./banner";
 import { ProjectSection } from "./projects-section";
+import { TemplatesSection } from "./templates-section";
 
 export default async function Home() {
   
   await protectServer();
-  const session = await auth()
 
   return (
     <div className="flex flex-col space-y-6 max-w-7xl mx-auto pb-10">
       <Banner />
+      <TemplatesSection />
       <ProjectSection />
     </div>
   );
